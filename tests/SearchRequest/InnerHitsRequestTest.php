8c440739a25123d9da66678aad361a1ad81ce7cf
Gytis Škėma <gytis.skema@gmail.com>
2018-03-04 20:32:56 +0200

    Add test cases for search, inner and top hits requests

--- tests/SearchRequest/InnerHitsRequestTest.php
+++ tests/SearchRequest/InnerHitsRequestTest.php
@@ -3,6 +3,11 @@
 namespace Gskema\ElasticSearchQueryDSL\SearchRequest;
 
 use Gskema\ElasticSearchQueryDSL\AbstractJsonSerializeTest;
+use Gskema\ElasticSearchQueryDSL\Highlighter\Highlighter;
+use Gskema\ElasticSearchQueryDSL\Model\Script\InlineScript;
+use Gskema\ElasticSearchQueryDSL\Sorter\FieldSorter;
+use Gskema\ElasticSearchQueryDSL\Sorter\RawSorter;
+use Gskema\ElasticSearchQueryDSL\SourceFilter\DisabledSourceFilter;
 
 class InnerHitsRequestTest extends AbstractJsonSerializeTest
 {
@@ -24,6 +29,59 @@ class InnerHitsRequestTest extends AbstractJsonSerializeTest
                 "size": 1
             }',
             (new InnerHitsRequest())->setSize(1),
+        ];
+
+        // #2
+        $dataSets[] = [
+            // language=JSON
+            '{
+                "explain": true,
+                "version": true,
+                "_source": false,
+                "script_fields": {
+                    "scriptField1": "script1",
+                    "scriptField2": "script2",
+                    "scriptField3": "script3"
+                },
+                "docvalue_fields": ["docValueField1", "docValueField2"],
+                "from": 10,
+                "size": 5,
+                "sort": [
+                    { "field3": {"order": "desc"} },
+                    "field4"
+                ],
+                "highlight": {
+                    "fields": {
+                        "field5": {
+                            "order": "score"
+                        },
+                        "field6": {}
+                    }
+                }
+            }',
+            (new InnerHitsRequest())
+                ->setOptions([
+                    'explain' => true,
+                ])
+                ->setOption('version', true)
+                ->setSourceFields(new DisabledSourceFilter())
+                ->setScriptFields([
+                    'scriptField1' => new InlineScript('script1'),
+                    'scriptField2' => new InlineScript('script2'),
+                ])
+                ->setScriptField('scriptField3', new InlineScript('script3'))
+                ->setDocValueFields(['docValueField1', 'docValueField2'])
+                ->setFrom(10)
+                ->setSize(5)
+                ->setSorters([
+                    new FieldSorter('field3', 'desc')
+                ])
+                ->addSorter(new RawSorter('field4'))
+                ->setHighlighter(
+                    (new Highlighter())
+                        ->setField('field5', ['order' => 'score'])
+                        ->setField('field6')
+                )
         ];
 
         return $dataSets;
--- tests/SearchRequest/SearchRequestTest.php
+++ tests/SearchRequest/SearchRequestTest.php
@@ -3,6 +3,22 @@
 namespace Gskema\ElasticSearchQueryDSL\SearchRequest;
 
 use Gskema\ElasticSearchQueryDSL\AbstractJsonSerializeTest;
+use Gskema\ElasticSearchQueryDSL\Aggregation\Bucket\FilterAggregation;
+use Gskema\ElasticSearchQueryDSL\Aggregation\Bucket\TermsAggregation;
+use Gskema\ElasticSearchQueryDSL\Aggregation\Metric\MaxAggregation;
+use Gskema\ElasticSearchQueryDSL\FieldCollapser\FieldCollapser;
+use Gskema\ElasticSearchQueryDSL\Highlighter\Highlighter;
+use Gskema\ElasticSearchQueryDSL\Matcher\MatchAllMatcher;
+use Gskema\ElasticSearchQueryDSL\Matcher\MatchNoneMatcher;
+use Gskema\ElasticSearchQueryDSL\Matcher\TermLevel\TermMatcher;
+use Gskema\ElasticSearchQueryDSL\Matcher\TermLevel\WildcardMatcher;
+use Gskema\ElasticSearchQueryDSL\Model\Script\InlineScript;
+use Gskema\ElasticSearchQueryDSL\Rescorer\QueryRescorer;
+use Gskema\ElasticSearchQueryDSL\Sorter\FieldSorter;
+use Gskema\ElasticSearchQueryDSL\Sorter\RawSorter;
+use Gskema\ElasticSearchQueryDSL\SourceFilter\DisabledSourceFilter;
+use Gskema\ElasticSearchQueryDSL\Suggester\PhraseSuggester;
+use Gskema\ElasticSearchQueryDSL\Suggester\TermSuggester;
 
 class SearchRequestTest extends AbstractJsonSerializeTest
 {
@@ -24,6 +40,139 @@ class SearchRequestTest extends AbstractJsonSerializeTest
                 "size": 1
             }',
             (new SearchRequest())->setSize(1),
+        ];
+
+        // #2
+        $dataSets[] = [
+            // language=JSON
+            '{
+                "explain": true,
+                "version": true,
+                "_source": false,
+                "stored_fields": ["storedField1", "storedField2"],
+                "script_fields": {
+                    "scriptField1": "script1",
+                    "scriptField2": "script2",
+                    "scriptField3": "script3"
+                },
+                "docvalue_fields": ["docValueField1", "docValueField2"],
+                "from": 10,
+                "size": 5,
+                "query": {
+                    "term": {
+                        "field1": "value1"
+                    }
+                },
+                "post_filter": {
+                    "wildcard": {
+                        "field2": "value*"
+                    }
+                },
+                "sort": [
+                    { "field3": {"order": "desc"} },
+                    "field4"
+                ],
+                "rescore": [
+                    {
+                        "query": {
+                            "rescore_query": { "match_all": {} },
+                             "score_mode": "avg"
+                        }
+                    },
+                    {
+                        "query": {
+                            "rescore_query": { "match_none": {} }
+                        }
+                    }
+                ],
+                "highlight": {
+                    "fields": {
+                        "field5": {
+                            "order": "score"
+                        },
+                        "field6": {}
+                    }
+                },
+                "suggest": {
+                    "suggesterKey1": {
+                        "text": "text1",
+                        "term": {
+                            "field": "field7"
+                        }
+                    },
+                    "suggesterKey2": {
+                        "text": "text2",
+                        "phrase": {
+                            "field": "field8"
+                        }
+                    }
+                },
+                "stats": ["statGroup1", "statGroup2", "statGroup3"],
+                "collapse": {
+                    "field": "field9"
+                },
+                "aggs": {
+                    "agg1": {
+                        "filter": { "match_all": {} },
+                        "aggs": {
+                            "agg2": {
+                                "terms": {
+                                    "field": "field10"
+                                }
+                            }
+                        }
+                    },
+                    "agg3": {
+                        "max": {
+                            "field": "field11"
+                        }
+                    }
+                }
+            }',
+            (new SearchRequest())
+            ->setOptions([
+                'explain' => true,
+            ])
+            ->setOption('version', true)
+            ->setSourceFields(new DisabledSourceFilter())
+            ->setStoredFields(['storedField1', 'storedField2'])
+            ->setScriptFields([
+                'scriptField1' => new InlineScript('script1'),
+                'scriptField2' => new InlineScript('script2'),
+            ])
+            ->setScriptField('scriptField3', new InlineScript('script3'))
+            ->setDocValueFields(['docValueField1', 'docValueField2'])
+            ->setFrom(10)
+            ->setSize(5)
+            ->setQuery(new TermMatcher('field1', 'value1'))
+            ->setPostFilter(new WildcardMatcher('field2', 'value*'))
+            ->setSorters([
+                new FieldSorter('field3', 'desc')
+            ])
+            ->addSorter(new RawSorter('field4'))
+            ->setRescorers([
+                (new QueryRescorer(new MatchAllMatcher()))->setScoreMode('avg')
+            ])
+            ->addRescorer(new QueryRescorer(new MatchNoneMatcher()))
+            ->setHighlighter(
+                (new Highlighter())
+                    ->setField('field5', ['order' => 'score'])
+                    ->setField('field6')
+            )
+            ->setSuggesters([
+                'suggesterKey1' => new TermSuggester('field7', 'text1')
+            ])
+            ->setSuggester('suggesterKey2', new PhraseSuggester('field8', 'text2'))
+            ->setStatGroups(['statGroup1', 'statGroup2'])
+            ->addStatGroup('statGroup3')
+            ->setFieldCollapser(
+                (new FieldCollapser('field9'))
+            )
+            ->setAggs([
+                'agg1' => (new FilterAggregation(new MatchAllMatcher()))
+                          ->setAgg('agg2', TermsAggregation::fromField('field10'))
+            ])
+            ->setAgg('agg3', MaxAggregation::fromField('field11'))
         ];
 
         return $dataSets;
