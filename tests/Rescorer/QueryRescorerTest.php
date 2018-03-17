e1b2d91557e2c7f5ce6fc4146e5941f172521167
Gytis Škėma <gytis.skema@gmail.com>
2018-03-17 13:44:56 +0200

    Expand test in QueryRescorerTest

--- tests/Rescorer/QueryRescorerTest.php
+++ tests/Rescorer/QueryRescorerTest.php
@@ -14,7 +14,11 @@ class QueryRescorerTest extends AbstractJsonSerializeTest
         $dataSets[] = [
             // language=JSON
             '{
+                "window_size": 10,
                 "query": {
+                    "query_weight": 1.0,
+                    "rescore_query_weight": 2.0,
+                    "score_mode": "avg",
                     "rescore_query": {
                         "term": {
                             "field1": "value1"
@@ -23,8 +27,27 @@ class QueryRescorerTest extends AbstractJsonSerializeTest
                 }
             }',
             (new QueryRescorer(new TermMatcher('field1', 'value1')))
+                ->setQueryWeight(1.0)
+                ->setRescoreQueryWeight(2.0)
+                ->setScoreMode('avg')
+                ->setWindowSize(10),
         ];
 
         return $dataSets;
+    }
+
+    public function testMethods()
+    {
+        $rescorer = (new QueryRescorer(new TermMatcher('field1', 'value1')))
+            ->setQueryWeight(1.0)
+            ->setRescoreQueryWeight(2.0)
+            ->setScoreMode('avg')
+            ->setWindowSize(10);
+
+        $this->assertEquals(new TermMatcher('field1', 'value1'), $rescorer->getQuery());
+        $this->assertEquals(1.0, $rescorer->getQueryWeight());
+        $this->assertEquals(2.0, $rescorer->getRescoreQueryWeight());
+        $this->assertEquals('avg', $rescorer->getScoreMode());
+        $this->assertEquals(10, $rescorer->getWindowSize());
     }
 }
